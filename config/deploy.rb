91ec5e1203b0cdb5985194d6a3a0b8bb4c0612f3
Sylvain Santucci <sylvain.santucci@gmail.com>
2021-02-26 10:11:22 +0100

    Update

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,7 +47,7 @@ namespace :deploy do
   task :import_lua do
     on roles(:web) do
       within release_path do
-        execute("cd #{release_path} && rails import_lua")
+        execute("cd #{current_path} && rails import_lua")
       end
     end
   end
