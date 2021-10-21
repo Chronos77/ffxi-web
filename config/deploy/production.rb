343d0cd35d36a27eb833567733f8af28f429530c
Sylvain Santucci <sylvain.santucci@gmail.com>
2021-10-21 23:07:25 +0200

    Update env

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "192.168.1.82", user: "ubuntu", roles: %w{app db web}
+server "192.168.1.21", user: "ubuntu", roles: %w{app db web}
 set :rvm_custom_path, '/usr/share/rvm'
 set :rvm_ruby_version, '2.5.8'
 
